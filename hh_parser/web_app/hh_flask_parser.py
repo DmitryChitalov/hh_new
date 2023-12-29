994102b87a9a5df3c0b10c395c171e1e5aa1cfef
DmitryChitalov <cdi9@yandex.ru>
2023-12-29 23:15:06 +0500

    !

--- hh_parser/web_app/hh_flask_parser.py
+++ hh_parser/web_app/hh_flask_parser.py
@@ -6,7 +6,27 @@ import json
 sys.path.append(os.path.join(os.getcwd(), '..'))
 import multiprocessing as ml
 from parser_app import main as pr
-from web_app import create_app
+#from web_app import create_app
+
+from flask import Flask, render_template
+
+SECRET_KEY = b'\x143#\x1eV;\xc9\xa0\xecr\r\xd4/{b\n'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def create_app():
+    from hh_parser.web_app.flask_parser.flask_parser import parser_blueprint
+    from hh_parser.web_app.authorization.auth import auth_blueprint
+    app.register_blueprint(parser_blueprint)
+    app.register_blueprint(auth_blueprint)
+
+    return app
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
 
 if __name__ == "__main__":
     # Создать приложение Flask
