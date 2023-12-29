4c882afc9934acd110146201ef3dd6d06164e263
DmitryChitalov <cdi9@yandex.ru>
2023-12-29 23:34:56 +0500

    !

--- hh_parser/web_app/hh_flask_parser.py
+++ hh_parser/web_app/hh_flask_parser.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def create_app():
-    from hh_parser.web_app.flask_parser.flask_parser import parser_blueprint
-    from hh_parser.web_app.authorization.auth import auth_blueprint
+    from web_app.flask_parser.flask_parser import parser_blueprint
+    from web_app.authorization.auth import auth_blueprint
     app.register_blueprint(parser_blueprint)
     app.register_blueprint(auth_blueprint)
 
